caa79f6ae22809dfe1c861830258f0dc4d916840
Victor Rosas <victorrosas@victors-macbook-pro.local>
2018-10-08 09:38:05 -0700

    Initial Commit

---
+++ CoreDataSimpleUserStorageSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataSimpleUserStorageSwift
+//
+//  Created by Victor Rosas on 10/8/18.
+//  Copyright © 2018 Victor Rosas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
